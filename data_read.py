db3cece26a9ae19ae02cd83e935cd4efde6bbe2f
lisa_qiu <lisa_qiu@qq.com>
2019-02-18 09:20:32 +0800

    第五周读取excel

--- data_read.py
+++ data_read.py
@@ -1,3 +1,15 @@
 # -*- coding:utf-8 -*
+import numpy as np 
+import xlrd
+import pandas as pd 
+import sys
+from pandas import Series, DataFrame
 
 #读入课程资源中作业数据的所有数据集
+dataAs = pd.read_excel('05/ApplianceShipments.xls') 
+
+list(open('05/creditcard-dataset.txt'))
+dataCd = pd.read_table('05/creditcard-dataset.txt',sep='\s+')
+
+print(dataAs)
+# print(dataCd)
