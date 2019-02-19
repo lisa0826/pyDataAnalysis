3c85e80417f3e92dcc8a4b4fbb14a4fef85f118f
lisa <lisa_qiu@qq.com>
2019-02-19 18:45:34 +0800

    第六周作业完成

--- 06/data_opt.py
+++ 06/data_opt.py
@@ -11,14 +11,55 @@ df1['new_dj']=pd.qcut(df1['New_Values'], 5, precision=2)
 new_dl=df1['New_Values'].groupby(df1['new_dl'])
 new_dj=df1['New_Values'].groupby(df1['new_dj'])
  
-print(new_dl.median())  #等距方式求中位数
-print(new_dl.std())  #等距方式求标准差
-print(new_dj.median())  #等距等量求中位数
-print(new_dj.std())  #等距等量求标准差
+# print(new_dl.median())  #等距方式求中位数
+# print(new_dl.std())  #等距方式求标准差
+# print(new_dj.median())  #等距等量求中位数
+# print(new_dj.std())  #等距等量求标准差
+
 
 #2. 读入BHP1.csv，使用适当的方法填补缺失值
+from scipy.interpolate import lagrange
+###缺失值处理——拉格朗日插值法
+inputfile = 'BHP1.csv' 
+outputfile = 'NEWBHP1.csv' 
+
+data = pd.read_csv(inputfile) 
+
+def ployinterp_column(s, n, k=10):
+  y = s[list(range(n-k, n)) + list(range(n+1, n+1+k))] 
+  y = y[y.notnull()]
+  return lagrange(y.index, list(y))(n)
+
+for i in data.columns:
+  for j in range(len(data)):
+    if (data[i].isnull())[j]: 
+      data[i][j] = ployinterp_column(data[i], j)
+
+# data.to_csv(outputfile)
 
 
 #3. 读入BHP2.xlsx，与BHP1数据集合并为BHP数据集
+df3_BHP1=pd.read_csv('BHP1.csv')
+df3_BHP2=pd.read_excel('BHP2.xlsx')
+
+df3_BHP=pd.merge(df3_BHP1, df3_BHP2, how='outer')
+
+# print(df3_BHP)
+
 
 #4. 将BHP数据集中的成交量（volume）替换为 high、median、low 三种水平（区间自行定义）
+
+volume = df3_BHP['volume']
+ 
+idx = 0
+for i in volume:
+    if i >= 2000000 and i < 3000000:
+        df3_BHP['volume'][idx] = "median"
+    elif i >= 3000000 :
+        df3_BHP['volume'][idx] = "high"
+    else: 
+        df3_BHP['volume'][idx] = "low "
+    idx+=1
+    
+print(df3_BHP)
+
