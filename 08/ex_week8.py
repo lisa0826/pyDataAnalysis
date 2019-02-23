678b265ba337f5907cecfc4d23d1c2351210eaa7
lisa_qiu <lisa_qiu@qq.com>
2019-02-23 21:16:56 +0800

    第八周作业

---
+++ 08/ex_week8.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+from pandas import Series, DataFrame
+import pandas as pd
+
+### GroupBy 技术
+df = DataFrame({'key1' : ['a', 'a', 'b', 'b', 'a'],
+                'key2' : ['one', 'two', 'one', 'two', 'one'],
+                'data1' : np.random.randn(5),
+                'data2' : np.random.randn(5)})
+print(df)
+
+grouped = df['data1'].groupby(df['key1'])
+print(grouped)
