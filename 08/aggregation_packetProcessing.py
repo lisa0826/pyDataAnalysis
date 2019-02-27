818c8a9681a1a772fb4ff408e0ff6fb282e8e51d
lisa <lisa_qiu@qq.com>
2019-02-27 15:13:43 +0800

    第八周第一题

--- 08/aggregation_packetProcessing.py
+++ 08/aggregation_packetProcessing.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
-
-
+import pandas as pd
+import numpy as np
 
 # 读入tips.csv 数据集
+data=pd.read_csv("tips.csv")
 
+dataTip = data['tip']
 
 # 1. 统计不同time的tip的均值，方差
+print(np.mean(dataTip))
+print(np.var(dataTip))
 
 # 2. 将total_bill和tip根据不同的sex进行标准化(原数据减去均值的结果除以标准差)
+dataTip = data['sex']
 
 # 3. 计算吸烟者和非吸烟者的小费比例值均值  的差值
 
