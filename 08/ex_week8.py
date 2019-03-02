bca9f073637a76367d65b798a7e8fe278780f391
lisa_qiu <lisa_qiu@qq.com>
2019-03-02 23:17:42 +0800

    第八周练习
    面向列的多函数应用

--- 08/ex_week8.py
+++ 08/ex_week8.py
@@ -11,4 +11,50 @@ df = DataFrame({'key1' : ['a', 'a', 'b', 'b', 'a'],
 # print(df)
 
 grouped = df['data1'].groupby(df['key1'])
-print(grouped.mean())
+# print(grouped.mean())
+
+means1 = df['data1'].groupby([df['key1'], df['key2']]).mean()
+means2 = df['data1'].groupby(df['key2']).mean()
+# print(means1)
+# print(means2)
+# print(means1.unstack())
+
+states = np.array(['Ohio', 'California', 'California', 'Ohio', 'Ohio'])
+years = np.array([2005, 2005, 2006, 2005, 2006])
+df['data1'].groupby([states, years]).mean()
+
+# print(df.groupby('key1').mean())
+# print(df.groupby(['key1', 'key2']).mean())
+# print(df.groupby(['key1', 'key2']).size())
+
+def peak_to_peak(arr):
+    return arr.max() - arr.min()
+grouped.agg(peak_to_peak)
+
+# ### 面向列的多函数应用
+tips = pd.read_csv('tips.csv')
+
+tips['tip_pct'] = tips['tip'] / tips['total_bill']
+tips[:6]
+
+grouped = tips.groupby(['sex', 'smoker'])
+grouped_pct = grouped['tip_pct']
+print(grouped_pct.agg('mean'))
+
+print(grouped_pct.agg(['mean', 'std', peak_to_peak]))
+
+print(grouped_pct.agg([('foo', 'mean'), ('bar', np.std)]))
+
+functions = ['count', 'mean', 'max']
+result = grouped['tip_pct', 'total_bill'].agg(functions)
+print(result)
+
+result['tip_pct']
+
+ftuples = [('Durchschnitt', 'mean'), ('Abweichung', np.var)]
+grouped['tip_pct', 'total_bill'].agg(ftuples)
+
+grouped.agg({'tip' : np.max, 'size' : 'sum'})
+
+grouped.agg({'tip_pct' : ['min', 'max', 'mean', 'std'],
+             'size' : 'sum'})
