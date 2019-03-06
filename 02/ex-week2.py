2b0e25e89dfa5cada3cf486f48ea0af6b950a90d
lisa <lisa_qiu@qq.com>
2019-03-06 16:57:33 +0800

    第二周练习

---
+++ 02/ex-week2.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+
+# flag = False
+# name = 'python'
+# if name == 'python':
+# 	flag = True
+# 	print('welcome boss')
+# else:
+# 	print(name)
+
+
+# num = 2     
+# if num == 3:            # 判断num的值
+#     print('boss')        
+# elif num == 2:
+#     print('user')
+# elif num == 1:
+#     print('worker')
+# elif num < 0:           # 值小于零时输出
+#     print('error')
+# else:
+#     print('roadman')    # 条件均不成立时输出
+
+# num = 9
+# if num >= 0 and num <= 10:    # 判断值是否在0~10之间
+#     print('hello')
+
+# num = 10
+# if num < 0 or num > 10:    # 判断值是否在小于0或大于10
+#     print('hello')
+# else:
+# 	print('undefine')
+
+# num = 8
+# #判断值是否在0~5或者10~15之间
+# if (num >= 0 and num <= 5) or (num >= 10 and num <= 15):
+# 	print('hello')
+# else:
+# 	print('undefine')
+
+# var = 100
+# if (var == 100):print('变量var的值为100')
+# print('Good bye!')
+
+
+#while语句
+'''
+while 判断条件：
+    执行语句……
+'''
+# count = 0
+# while (count < 9):
+#    print('The count is:', count)
+#    count = count + 1
+
+# print("Good bye!")
+
+
+# continue 和 break 用法
+# i = 1
+# while i < 10:   
+#     i += 1
+#     if i%2 > 0:     # 非双数时跳过输出
+#         continue
+#     print(i)       # 输出双数2、4、6、8、10
+
+# i = 1
+# while 1:            # 循环条件为1必定成立
+#     print(i)         # 输出1~10
+#     i += 1
+#     if i > 10:     # 当i大于10时跳出循环
+#         break
+
+#while … else 
+# count = 0
+# while count < 5:
+#    print(count, " is  less than 5")
+#    count = count + 1
+# else:
+#    print(count, " is not less than 5")
+
+#简单语句组
+# flag = 1
+# while (flag): print('Given flag is really true!'); #无限循环
+# flag=0;
+# print("Good bye!")
+
+# for letter in 'Python':     
+#    print('当前字母 :', letter)
+
+# fruits = ['banana', 'apple',  'mango']
+# for fruit in fruits:        
+#    print('当前水果 :', fruit)
+
+# print("Good bye!")
+
+#序列索引迭代
+# fruits = ['banana', 'apple',  'mango']
+# for index in range(len(fruits)):
+#    print('当前水果 :', fruits[index], index)
+
+# print("Good bye!")
+
+#for...else
+# for num in range(10,20):  # 迭代 10 到 20 之间的数字
+#    for i in range(2,num): # 根据因子迭代
+#       if num%i == 0:      # 确定第一个因子
+#          j=num/i          # 计算第二个因子
+#          print('%d 等于 %d * %d' % (num,i,j))
+#          break            # 跳出当前循环
+#    else:                  # 循环的 else 部分
+#       print(num, '是一个质数')
+
+#嵌套
+# i = 2
+# while(i < 100): #循环，判断100以内的素数
+#    j = 2
+#    while(j <= (i/j)):
+#       if not(i%j): break
+#       j = j + 1
+#    if (j > i/j) : print(i, " 是素数")
+#    i = i + 1
+
+# print("Good bye!")
+
+#break语句
+# for letter in 'Python':   # for循环的break语句
+#    if letter == 'h':
+#       break
+#    print('Current Letter :', letter)
+
+# var = 10                    
+# while var > 0:            # while循环的break语句
+#    print('Current variable value :', var)
+#    var = var -1
+#    if var == 5:
+#       break
+
+# print("Good bye!")
+
+#continue语句
+# for letter in 'Python':     # 对应break的第一个例子
+#    if letter == 'h':
+#       continue
+#    print('当前字母 :', letter)
+
+# var = 10                    # 对应break的第二个例子
+# while var > 0:              
+#    var = var -1
+#    if var == 5:
+#       continue
+#    print('当前变量值 :', var)
+# print("Good bye!")
+
+#pass语句
+# 输出 Python 的每个字母
+# for letter in 'Python':
+#    if letter == 'h':
+#       pass
+#       print('这是 pass 块')
+#    print('当前字母 :', letter)
+
+# print("Good bye!")
+
+#时间与日期
+import time;  # This is required to include time module.
+
+ticks = time.time()
+print("Number of ticks since 12:00am, January 1, 1970:", ticks)
+
+localtime = time.localtime(time.time())
+print("Local current time :", localtime)
+
+localtime = time.asctime( time.localtime(time.time()) )
+print("Local current time :", localtime)
+
+import calendar
+
+cal = calendar.month(2008, 1)
+print("Here is the calendar:")
+print(cal);
+
+
